400a7a449969093a15c0c864d5d62bda021fb35a
desenvolvimento1 <estagio.ti@piscinafacil.com.br>
2018-10-04 08:56:46 -0300

    aula 44

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,50 +1,14 @@
 import { makeExecutableSchema } from 'graphql-tools'
 
-const users: any = [
-    {
-        id: 1,
-        name: 'Jon',
-        email: 'jon@email.com'
-    },
-    {
-        id: 1,
-        name: 'Dany',
-        email: 'dany@email.com'
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
     }
-]
+`
 
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-    }
+export default makeExecutableSchema({
+    typeDefs: [
 
-    type Query {
-        allUsers: [User!]!
-    }
-
-    type Mutation {
-        createUser(name: String!, email: String!): User
-    }
-`;
-
-const resolvers = {
-    User: {
-        id: (user) => user.id,
-        name: (user) => user.name,
-        email: (user) => user.email
-    },
-    Query: {
-        allUsers: () => users
-    },
-    Mutation: {
-        createUser: (parent, args) => {
-            const newUser = Object.assign({id: users.length + 1}, args);
-            users.push(newUser);
-            return newUser;
-        }
-    }
-};
-
-export default makeExecutableSchema({typeDefs, resolvers});
+    ]
+});
