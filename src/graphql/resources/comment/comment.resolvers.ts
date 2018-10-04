b3843f6d8f1c5f7844be83679c7e56f7a4049eca
desenvolvimento1 <estagio.ti@piscinafacil.com.br>
2018-10-04 17:28:33 -0300

    aula 59

--- src/graphql/resources/comment/comment.resolvers.ts
+++ src/graphql/resources/comment/comment.resolvers.ts
@@ -1,5 +1,7 @@
 import { GraphQLResolveInfo } from "graphql";
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
+import { Transaction } from "sequelize";
+import { CommentInstance } from "../../../models/CommentModel";
 
 export const commentResolvers = {
 
@@ -27,5 +29,41 @@ export const commentResolvers = {
                     offset: offset
                 });
         }
+    },
+
+    Mutation: {
+        
+        createComment: (parent, {input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .create(input, {transaction: t})
+            });
+        },
+
+        updateComment: (parent, {id, input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment: CommentInstance) => {
+                        if(!comment) throw new Error(`Comment with id ${id} not found!`);
+                        return comment.update(input, {transaction: t})
+                    })
+            });
+        },
+
+        deleteComment: (parent, {id,}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment: CommentInstance) => {
+                        if(!comment) throw new Error(`Comment with id ${id} not found!`);
+                        return comment.destroy({transaction: t})
+                            .then(comment => !!comment);
+                    })
+            });
+        }
+
     }
 }
