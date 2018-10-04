159273f0186175ba59fb90a8cf5a038d6fcb8f0f
desenvolvimento1 <estagio.ti@piscinafacil.com.br>
2018-10-04 17:46:04 -0300

    fix invalid sintaxe

--- src/graphql/resources/user/user.schema.ts
+++ src/graphql/resources/user/user.schema.ts
@@ -8,7 +8,7 @@ const userTypes = `
         photo: String
         createdAt: String!
         updatedAt: String!
-        posts(first: Int, offset: Int): [ Posts! ]!
+        posts(first: Int, offset: Int): [ Post! ]!
     }
 
     input UserCreateInput {
