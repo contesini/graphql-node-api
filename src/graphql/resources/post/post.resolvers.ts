e7e647e94b69b76fc5744818ae5472130e1a99a3
desenvolvimento1 <estagio.ti@piscinafacil.com.br>
2018-10-06 17:38:27 -0300

    aula 98

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,3 +1,4 @@
+import * as graphqlFields from 'graphql-fields';
 import { GraphQLResolveInfo } from "graphql";
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { PostInstance } from "../../../models/PostModel";
@@ -33,6 +34,7 @@ export const postResolvers = {
     Query: {
 
         posts: (parent, { first = 10, offset = 0 }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+            console.log(Object.keys(graphqlFields(info)))
             return db.Post.findAll({
                 limit: first,
                 offset: offset
